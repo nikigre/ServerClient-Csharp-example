1981cd926561ffb79267b2ccd851a9e3d712e1d8
nikigre <nik@nikigre.si>
2021-05-06 11:20:54 +0200

    UDP Server/Client

---
+++ UDP/Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = null;
+            try
+            {
+                //Connect to a server
+                client = new UdpClient("127.0.0.1", 200);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem connecting to a remote server. Maybe it is offline?\nException: " + ex.Message);
+                Environment.Exit(1);
+            }
+
+            //New thread for printing data from server
+            Thread t = new Thread(new ParameterizedThreadStart(WriteToConsole));
+            t.Start(client);
+
+            while (true)
+            {
+                string thing = System.Console.ReadLine();
+
+                //When we read a new line of text from console, we encode it to UTF-8,
+                byte[] thingByte = Encoding.UTF8.GetBytes(thing);
+
+
+                //and send all data to our server
+                client.Send(thingByte, thingByte.Length);
+            }
+        }
+
+        private static void WriteToConsole(object clientObject)
+        {
+            //Gets the client and the stream for the client
+            UdpClient sender = (UdpClient)clientObject;
+
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                while (true)
+                {
+                    //This variable will contain text we got from client
+                    string dataString;
+
+                    //data is our buffer
+                    byte[] data = new byte[1024];
+
+                    //We are using MemoryStream for saving recieved bytes which is muc easier to use that arrays
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+
+                        byte[] dataRead = sender.Receive(ref iPEndPoint);
+                        ms.Write(dataRead, 0, dataRead.Length);//We write all bytes from 0 to numBytesRead and save them in memory stream
+
+                        //When we are done with reading data, then we can get UTF-8 string from our memory stream
+                        dataString = Encoding.UTF8.GetString(ms.ToArray(), 0, (int)ms.Length);
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("\nServer message: " + dataString + "\n");
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection with client has been lost.\nException: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ UDP/Server/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new server object
+            Server server = new Server(200);
+
+            server.RunServer(); //We run our server
+        }
+    }
+
+    class Server
+    {
+        /// <summary>
+        /// TcpListener that is waiting for clients to connect
+        /// </summary>
+        UdpClient serverUDP = null;
+
+        byte[] welcomeMessage = Encoding.UTF8.GetBytes("Wellcome client! :)");
+
+        /// <summary>
+        /// Creates a new instance of server
+        /// </summary>
+        /// <param name="port">On which port you want to start server</param>
+        public Server(int port)
+        {
+            //Initialize variable
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, port);
+            serverUDP = new UdpClient(ipep);
+        }
+
+        /// <summary>
+        /// Method runs a server
+        /// </summary>
+        public void RunServer()
+        {
+            try
+            {
+                //Starts new thread that listens for clients
+                Thread thread = new Thread(new ThreadStart(StartListening));
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem starting a web server. " + ex.Message);
+                Environment.Exit(1);
+            }
+        }
+
+        /// <summary>
+        /// This method starts listening for new clients
+        /// </summary>
+        private void StartListening()
+        {
+            try
+            {
+                while (true)
+                {
+                    //This variable will hold info about our client that sent and UDP packet
+                    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+                    Console.WriteLine("Waiting for a connection...");
+
+                    //Here we are waiting for a client to send us something. 
+                    //Notice, that we used ref sender. sender variable parameters will be changed depending on the new client
+                    byte[] dataFromClient = serverUDP.Receive(ref sender); //Here we are waiting for a new connection
+
+                    Console.WriteLine("Message from UDP IP: " + sender);
+
+                    //We convert receieved bytes to string
+                    string dataString = Encoding.UTF8.GetString(dataFromClient);
+
+                    //Here, we encode received text back to UTF-8 bytes
+                    byte[] content = Encoding.UTF8.GetBytes(dataString);
+
+                    //And here, we send the same data back.
+                    serverUDP.Send(content, content.Length, sender);
+                }
+            }
+            catch (SocketException e) //If anything goes wrong, then we print an error to the terminal and start listening again
+            {
+                Console.WriteLine("An error happened.\nSocketException: {0}", e.Message);
+                StartListening();
+            }
+        }
+    }
+}
